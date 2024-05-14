78d3a0b35a21c94dcaf7f32eb46abf1c24409a93
hasanulgic <hasanulgic@gmail.com>
2024-05-14 16:58:34 +0600

    guide section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -50,7 +50,7 @@ export default function Hero() {
           />
         </div>
       </div>
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex flex-1 items-start scale-up-center">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween ">
