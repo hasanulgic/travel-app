4171f40575bceba52512511d84b5b29fb0b145e9
hasanulgic <hasanulgic@gmail.com>
2024-05-15 12:56:34 +0600

    footer done

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function GetApp() {
   return (
-    <section className="border-2 border-red-500 flexCenter w-full flex-col pb-[100px]">
+    <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 w-full flex flex-1 flex-col items-start justify-center gap-12">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
